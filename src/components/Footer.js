acc4074d9c6716c35417e90d4864be72d756752f
emirsgithub <eaa.emirr@gmail.com>
2025-03-13 15:56:20 +0000

    added bmac

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 const Footer = () => {
   return (
     <footer className="bg-[#070707] p-12 text-center text-gray-400 animate__animated animate__fadeInUp" style={{ animationDuration: '1.7s' }}>
@@ -8,6 +7,7 @@ const Footer = () => {
       <div className="mt-6 space-x-8">
         <Link to="/privacy" className="text-gray-400 hover:text-gray-300 transition duration-200 text-lg">Privacy Policy</Link>
         <Link to="/terms" className="text-gray-400 hover:text-gray-300 transition duration-200 text-lg">Terms of Service</Link>
+        <a href="www.buymeacoffee.com/thesubjectsage" target="_blank" className="text-gray-400 hover:text-gray-300 transition duration-200 text-lg" >Buy Me A Coffee</a>
       </div>
     </footer>
   );
